068287fcb04c67dd4b6b234486abaa550f1a392f
togrulseyid <togrul.seyid93@gmail.com>
2016-10-23 23:51:56 +0200

    HelloUser
    Hello User class added

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -13,6 +13,11 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2016 - how are you? :)");
+
+
+		System.out.printf("Please enter your name: ");
+		HelloUser helloUser = new HelloUser("togrulseyid");
+		helloUser.greetUser();
 	}
 
 }
